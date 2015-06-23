753dcfb026f0f9990ee4c08cc034d2863a513fe2
Vinicius Silveira <viniciussilveira6@gmail.com>
2015-06-22 20:31:53 -0400

    #4 - Finalizado Layout da tela inicial

--- apps/backend/controllers/LoginController.php
+++ apps/backend/controllers/LoginController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Class and Function List:
  * Function list:
@@ -9,64 +10,66 @@
  * Classes list:
  * - LoginController extends SetupController
  */
+
 namespace Multiple\Backend\Controllers;
-use Multiple\Backend\Models\Users AS Users,  \Phalcon\Session\Adapter\Files as Session;
+
+use Multiple\Backend\Models\Users AS Users,
+    \Phalcon\Session\Adapter\Files as Session;
 
 class LoginController extends SetupController {
-	
-	public function indexAction() {
-		
-		session_start();
-		if ($_SESSION['user_login'] != NULL) {
-			$this->dispatcher->forward(array("controller" => 'settings', "action" => 'index'));
-		}
-		else {
-			$this->view->render('login', 'index');
-		}
-	}
-	
-	/**
-	 * Efetua o login no sistema
-	 * @return json_encode array para o jquery
-	 */
-	public function loginAction() {
-		
-		$this->view->disable();
-		
-		$users       = new Users();
-		
-		$user_login  = $this->request->getPost('user_login');
-		$user_passwd = md5($this->request->getPost('user_passwd'));
-		$success     = $users::findFirst("(user_login = '$user_login' OR user_email = '$user_login') AND user_passwd = '$user_passwd'");
-		if ($success) {
-			$this->creatSession($user_login);
-			$data['success'] = true;
-		} 
-		else {
-			$data['success'] = false;
-			$data['message'] = 'Usuário ou senha invalido!';
-		}
-		
-		echo json_encode($data);
-	}
-	
-	/**
-	 * Inicia a sessão do usuário ao efetuar o login
-	 * @param  string $user_login   nome de usuário
-	 * @return
-	 */
-	public function creatSession($user_login) {
-		session_start();
-		
-		$_SESSION['user_login'] = $user_login;
-	}
-	
-	/**
-	 * Destroi a sessão e redireciona para tela de login
-	 * @return
-	 */
-	public function logoff() {
-		session_destroy();
-		$this->view->render('login', 'index');
-	}
+
+    public function indexAction() {
+
+        session_start();
+        if ($_SESSION['user_login'] != NULL) {
+            $this->dispatcher->forward(array("controller" => 'settings', "action" => 'index'));
+        } else {
+            $this->view->render('login', 'index');
+        }
+    }
+
+    /**
+     * Efetua o login no sistema
+     * @return json_encode array para o jquery
+     */
+    public function loginAction() {
+
+        $this->view->disable();
+
+        $users = new Users();
+
+        $user_login = $this->request->getPost('user_login');
+        $user_passwd = md5($this->request->getPost('user_passwd'));
+        $success = $users::findFirst("(user_login = '$user_login' OR user_email = '$user_login') AND user_passwd = '$user_passwd'");
+        if ($success) {
+            $this->creatSession($user_login);
+            $data['success'] = true;
+        } else {
+            $data['success'] = false;
+            $data['message'] = 'Usuário ou senha invalido!';
+        }
+
+        echo json_encode($data);
+    }
+
+    /**
+     * Inicia a sessão do usuário ao efetuar o login
+     * @param  string $user_login   nome de usuário
+     * @return
+     */
+    public function creatSession($user_login) {
+        session_start();
+
+        $_SESSION['user_login'] = $user_login;
+    }
+
+    /**
+     * Destroi a sessão e redireciona para tela de login
+     * @return
+     */
+    public function logoff() {
+        session_destroy();
+        $this->view->render('login', 'index');
+    }
+
 }
--- apps/backend/controllers/SettingsController.php
+++ apps/backend/controllers/SettingsController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Class and Function List:
  * Function list:
@@ -6,40 +7,44 @@
  * Classes list:
  * - SettingsController extends \
  */
+
 namespace Multiple\Backend\Controllers;
-use Multiple\Backend\Models\Users AS Users, 
-	\Phalcon\Mvc\View AS View,
-	\Phalcon\Mvc\Model\Query;
+
+use Multiple\Backend\Models\Users,
+    Phalcon\Http\Response;
+
 class SettingsController extends \Phalcon\Mvc\Controller {
-	
-	public function indexAction() {
-		session_start();
-		/**
-                 * @todo: 
-                 * => Variáveis:
-                 * $blog (boolean) => true caso exista um blog, false caso não exista
-                 * $permissao (char) => Nível de permissão do usuário logado
-                 * $img_user (string) => caminho para imagem de usuário (caso exista)
-                 *      se não existir inserir caminho para imagem padrão
-                 * 
-                 * => Funcionalidades:
-                 * google analitics => Verificar como integrar ao blog e criar relatórios/gráficos
-                 * redes sociais => Verificar como integrar ao blog
-                 * usuários online => Como contar a quantidade de usuários online? É possível pelo analitics?
-                 * 
-                 * Verificar por que a validação de Sessão aparentemente não está funcionando.
-                 * Verificar o que mais é necessário para index
-                 */
-		if ($_SESSION['user_login'] != NULL) {
-			$users = new Users();
-			$user = explode(" ", $users->getUser($user_login));
-                        $vars['user'] = $user[0];
-			
-			$this->view->setVars($vars);
-			$this->view->pick('settings/index');
-		}
-		else {
-			$this->view->render('login', 'index');
-		}
-	}
+
+    public function indexAction() {
+
+        $response = new Response();
+        session_start();
+        /**
+         * @todo: 
+         * => Variáveis:
+         * $blog (boolean) => true caso exista um blog, false caso não exista
+         * $permissao (char) => Nível de permissão do usuário logado
+         * $img_user (string) => caminho para imagem de usuário (caso exista)
+         *      se não existir inserir caminho para imagem padrão
+         * 
+         * => Funcionalidades:
+         * google analitics => Verificar como integrar ao blog e criar relatórios/gráficos
+         * redes sociais => Verificar como integrar ao blog
+         * usuários online => Como contar a quantidade de usuários online? É possível pelo analitics?
+         * 
+         * Verificar por que a validação de Sessão aparentemente não está funcionando.
+         * Verificar o que mais é necessário para index
+         */
+        if ($_SESSION['user_login'] != NULL) {
+            $users = new Users();
+            $user = explode(" ", $users->getUser($_SESSION['user_login']));
+            $vars['user'] = $user[0];
+
+            $this->view->setVars($vars);
+            $this->view->render('settings', 'index');
+        } else {
+            $response->redirect("login/index");
+        }
+    }
+
 }
