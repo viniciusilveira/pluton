592795a68f530d1b116c1bb0a285b5e56c0dbf6b
viniciusilveira <viniciussilveira6@gmail.com>
2015-08-22 21:58:18 -0400

    Página da postagem e comentários via facebook

--- apps/frontend/controllers/IndexController.php
+++ apps/frontend/controllers/IndexController.php
@@ -4,6 +4,9 @@
  * Function list:
  * - indexAction()
  * - notFoundAction()
+ * - nextPageAction()
+ * - previusPageAction()
+ * - postPageAction()
  * Classes list:
  * - IndexController extends \
  */
@@ -24,10 +27,24 @@ class IndexController extends \Phalcon\Mvc\Controller {
         $this->session->start();
         $blog = Blogs::find();
         $vars['layout'] = Layouts::findFirst();
-        $vars['posts'] = Posts::findByPost_status_id(1);
-        foreach($vars['posts'] as $post){
-            //echo $post->post_content . "<br/><br/><br/>";
-        } //die();
+
+        $conditions = "post_status_id = :status:";
+        $bind = array(
+            "status" => 1
+        );
+        $order = "post_date_posted DESC";
+        $vars['posts'] =Posts::find(array(
+            "conditions" => $conditions,
+            "order" => $order,
+            "limit" => 10,
+            "bind" => $bind,
+        ));
+        foreach ($vars['posts'] as $post) {
+
+            $vars['post_title'][$post->post_id] = str_replace(" ", "-", $post->post_title, $count);
+
+        }
+
         $this->view->setVars($vars);
 
         //caso o blog esteja criado carrega a index; se não carrega a pagina not found
@@ -38,12 +55,19 @@ class IndexController extends \Phalcon\Mvc\Controller {
 
         //view/index/notFound.phtml
 
+
+    }
+
+    public function nextPageAction() {
+    }
+
+    public function previusPageAction() {
     }
 
     /**
      * Carrega a página de posts exibidindo a postagem informada via REQUEST
      */
-    public function postPageAction(){
+    public function postPageAction() {
         $post_id = $this->request->getPost("post_id");
 
         $post = Posts::findFirstByPost_id($post_id);
@@ -52,6 +76,5 @@ class IndexController extends \Phalcon\Mvc\Controller {
         $vars['layout'] = Layouts::findFirst();
         $vars['post'] = $post;
         $this->view->setVars($vars);
-
     }
 }
